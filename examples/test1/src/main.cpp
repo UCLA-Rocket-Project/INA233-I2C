f65000926f23b70c2f1ffe78d16343d95b924fad
lkross06 <lucaskalaniross@gmail.com>
2026-03-02 00:50:53 -0800

    feat: example test 1 code

--- examples/test1/src/main.cpp
+++ examples/test1/src/main.cpp
@@ -2,10 +2,31 @@
 #include <Wire.h>
 #include <INA233.h>
 
-void setup() {
+INA233 ina(
+    0.01f,  //example shunt resistance
+    10.0f,  //example maximum current rating
+    ADDRESS_PIN::GND,   //A0
+    ADDRESS_PIN::GND,   //A1
+    Wire
+);
 
+void setup() {
+    Serial.begin(115200);
+
+    if(!ina.begin()){
+        Serial.println("Setup failed.");
+        while(1);
+    }
+
+    Serial.println("Setup complete.");
 }
 
 void loop() {
+    int16_t raw_vin = ina.busVoltage_raw();
+    float vin = ina.busVoltage_V(raw_vin);
 
+    Serial.print("VIN = ");
+    Serial.println(vin);
+
+    delay(1000);
 }
